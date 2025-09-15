e2a539e31537a219778f0977bbc2d80443c7f8c8
rafaelcuba81 <130310854+rafaelcuba81@users.noreply.github.com>
2025-09-15 09:59:28 -0400

    Update Contact page image and fix About Us transparency

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -105,7 +105,7 @@ const ContactPage = () => {
         {/* Abstract Background Image */}
         <div className="absolute inset-0 w-full h-full">
           <img
-            src="https://images.pexels.com/photos/3184291/pexels-photo-3184291.jpeg?auto=compress&cs=tinysrgb&w=1200"
+            src="https://images.pexels.com/photos/3183153/pexels-photo-3183153.jpeg?auto=compress&cs=tinysrgb&w=1200"
             alt="Contact and communication"
             className="w-full h-full object-cover"
           />
